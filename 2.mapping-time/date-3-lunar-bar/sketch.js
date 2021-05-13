d1a171de29cb5b9e33d26ae7bb184509c9b9311c
FDTomato <78814326+FDTomato@users.noreply.github.com>
2021-05-12 23:59:49 -0400

    p2 update

--- 2.mapping-time/date-3-lunar-bar/sketch.js
+++ 2.mapping-time/date-3-lunar-bar/sketch.js
@@ -1,12 +1,14 @@
 function setup() {
   // set the width & height of the sketch
   createCanvas(700, 700)
- 
+  background(0)
 
 }
 
 function draw() {
+  //var now = clock()
  
+  rect(20,20,20)
 
   
 }
